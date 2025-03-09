0f4c56ec70f6645be27c6cc1c47d127399809056
akosinels <22BGU0003_ms@Psu.edu.ph>
2025-03-09 21:30:57 +0800

    Add project files.

---
+++ StudentForm.cs
@@ -0,0 +1,50 @@
+﻿using Aquino_JohnNelson_Act_GUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aquino_JohnNelson_Act_GUI
+{
+    public partial class StudentForm : Form
+    {
+        public StudentForm()
+        {
+            InitializeComponent();
+            // Mock Student Data
+            StudentNameLabel.Text = "John Nelson Aquino";
+            StudentAgeLabel.Text = "24";
+            StudentAddressLabel.Text = "123 Main Street";
+            StudentContactLabel.Text = "09123456789";
+            StudentEmailLabel.Text = "email@sample.com";
+            StudentCourseYearLabel.Text = "BSIT Third";
+            ParentNameLabel.Text = "Gina Aquino";
+            ParentContactLabel.Text = "09123456789";
+            HobbiesLabel.Text = "Programming, Gaming";
+            NicknameLabel.Text = "JN";
+        }
+
+        private void AddImageBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Add Image functionality is not implemented.", "Feature Not Available", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ChangeImageBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Change Image functionality is not implemented.", "Feature Not Available", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void EditUpdateBtn_Click(object sender, EventArgs e)
+        {
+            EditForm editForm = new EditForm(this);
+            this.Hide();
+            editForm.Show();
+        }
+
+    }
+}
